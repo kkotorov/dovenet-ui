080c271fb91e1ac47cf69f66b02a2f7c5d66a940
kkotorov <45800446+kkotorov@users.noreply.github.com>
2025-11-13 17:20:03 +0100

    backend on a server

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // change to your backend
+  baseURL: 'http://161.35.73.100:8080',
   withCredentials: true, // if using cookies
 });
 
