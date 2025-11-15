eefb868552ed5fa9f16451a11d2b748d85ce1c7a
kkotorov <45800446+kkotorov@users.noreply.github.com>
2025-11-15 19:54:43 +0100

    remove unused import

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useLocation, useNavigate } from "react-router-dom";
 import i18n from "../i18n";
