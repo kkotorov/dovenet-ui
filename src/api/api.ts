88525336efcc1d575cd65ad854260729b5cc5d73
kkotorov <45800446+kkotorov@users.noreply.github.com>
2026-02-05 00:39:32 +0100

    fix backend call

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'https://api.dovenet.eu/api', // Caddy HTTPS backend
-  baseURL:'http://localhost:8080/api',
+  baseURL: 'https://api.dovenet.eu/api', // Caddy HTTPS backend
+  //baseURL:'http://localhost:8080/api',
   withCredentials: true,
 });
 
